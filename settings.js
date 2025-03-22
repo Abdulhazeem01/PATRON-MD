a52a46103845b44adf924bd85a43788fa5439039
Abdulhazeem Morakinyo <130643850+Abdulhazeem01@users.noreply.github.com>
2025-03-22 05:39:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 //auto presence update
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "9067233220"
 
-global.bankowner = "FADARE"
+global.bankowner = "Abdulhazeem"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.wlcm = true
+global.wlcm = false 
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
